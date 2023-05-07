b98c37933d3e50ae1467b2acea17121e0561a05a
Jin seok Ko <38197122+kjs2016@users.noreply.github.com>
2023-05-07 23:09:06 +0900

    Update main.py

--- 보도자료자동화/main.py
+++ 보도자료자동화/main.py
@@ -1,4 +1,4 @@
-import 아시아투데이, 충청신문, 온아신문, 온양신문
+import 아시아투데이, 충청신문, 온아신문, 온양신문, 아산투데이
 from tqdm import tqdm
 import time
 
@@ -11,13 +11,17 @@ while True:
  else:
    break
  
-func_list = [아시아투데이.asiatoday, 충청신문.cc, 온아신문.onanews, 온양신문.ionyang]#각 모듈의 함수명 저장
+func_list = [아시아투데이.asiatoday0, 충청신문.cc1, 온아신문.onanews2, 온양신문.ionyang3, 아산투데이.asnatoday4]#각 모듈의 함수명 저장
 
 num_of_functions = len(func_list)#배열의 길이 저장
 
+
 result = ""
 for i in tqdm(range(num_of_functions)):
-   func_list[i](point)#배열에 있는 함수를 호출
-   time.sleep(0.5)  # 로딩바 속도를 조절하기 위한 지연 시간
-
+   try:
+      func_list[i](point)#배열에 있는 함수를 호출
+      time.sleep(0.5)  # 로딩바 속도를 조절하기 위한 지연 시간
+   except Exception as e:
+     print(f"{i}번째 함수 실행 중 오류가 발생했습니다 : {e}")
+     continue
 
